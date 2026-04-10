6f261a0a32dca8cec0a841a522b5f5bc5ac8aab9
Bhoomika shukla <bhoomikashukla@126.com>
2026-04-10 16:31:37 +0530

    vue js and laravel setup

---
+++ resources/js/components/App.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <h1>Hello Bhoomika 👋</h1>
+
+        <input v-model="name" placeholder="Enter name" />
+
+        <button @click="sayHello">Click</button>
+
+        <p>{{ message }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            name: '',
+            message: ''
+        }
+    },
+    methods: {
+        sayHello() {
+            this.message = "Hello " + this.name
+        }
+    }
+}
+</script>
